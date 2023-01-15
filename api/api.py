cc14633d5804adcf040b12fa7dcee6fdabe9bf4e
Tim <Tim.salokat@outlook.com>
2023-01-15 23:09:24 +0100

    Updated the files to work with the new frontend

---
+++ api/api.py
@@ -0,0 +1,165 @@
+from __future__ import annotations
+from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import os, sys
+
+import random
+
+from termcolor import colored
+
+app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "localhost:3000",
+    "http://127.0.0.1:3000",
+    "127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins = origins,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]    
+)
+
+Todos = []
+Projects = []
+
+def log(message, color="green"):
+    prefix = colored("[Back-Log]", color)
+    print(f"{prefix} - {message}")
+
+
+def Startup():
+    try:
+        Todos = eval(load_save("todos.txt"))
+        Projects = eval(load_save("projects.txt"))
+    except:
+        with open("saves/todos.txt", "w") as file:
+            file.write("[]")
+            file.close()
+            Todos=eval(load_save("todos.txt"))
+        with open("saves/projects.txt", "w") as file:
+            file.write("[]")
+            file.close()
+            Projects=eval(load_save("projects.txt"))
+
+    return Todos, Projects
+
+class Todo(BaseModel):
+    uuid: str
+    project_uuid: str
+
+    title: str
+    description: str
+    priority: int
+
+class Project(BaseModel):
+    uuid: str
+
+    title: str
+    description: str
+    total: int
+    finished: int
+
+    color: str
+
+def save(toSave,fileName):
+    path = "saves/" + fileName
+    with open(path, "w") as file:
+        file.write(str(toSave))
+        file.close()
+
+def load_save(fileName):
+    path = "saves/" + fileName
+    with open(path, "r") as file:
+        return file.read() 
+
+@app.get("/ping")
+async def ping():
+    log("Ping", "green")
+    return True
+
+@app.get("/get-todos")
+async def get_todos():
+    Todos = eval(load_save("todos.txt"))
+    return {"todos": Todos}
+
+@app.get("/get-projects")
+async def get_projects():
+    Projects = eval(load_save("projects.txt"))
+    return {"projects": Projects}
+
+@app.post("/add-todo")
+async def add_todo(todo: Todo):
+    print(todo)
+    Todos.append({
+        "uuid": todo.uuid,
+        "project_uuid": todo.project_uuid,
+        "title": todo.title,
+        "description": todo.description,
+        "priority": todo.priority,
+        "finished": False })
+    # update_index()
+    log(("Added Todo: ", todo.title))
+    save(Todos, "todos.txt")
+    return {"response": "Successful"}
+
+@app.post("/add-project")
+async def add_project(project: Project):
+    
+    hexadecimal = ["#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])]
+    Projects.append({
+        "uuid": project.uuid,
+        "title": project.title.title(),
+        "description": project.description,
+        "total": 0,
+        "finished": 0,
+        "color": hexadecimal,
+    })
+    save(Projects, "projects.txt")
+    log(("Added Project: ", project.title))
+    return {"response": "Successful"}
+
+@app.put("/set-finished")
+async def set_finished(uuid: str):
+    for todo in Todos:
+        if(todo["uuid"] == uuid):
+            todo["finished"] = not todo["finished"]
+    save(Todos, "todos.txt")
+    return {"response": "Successful"}
+
+@app.put("/edit-todo/{uuid}")
+async def edit_todo(uuid:str, todo: Todo):
+    for todo_item in Todos:
+        if(todo_item["uuid"] == uuid):
+            todo_item["heading"] = todo.heading
+            todo_item["description"] = todo.description
+            todo_item["project"] = todo.project
+    save(Todos, "todos.txt")
+    log(("Changed todo: " + todo.heading))
+    return {"response": "Successful"}
+
+@app.delete("/del-todo")
+async def del_todo(uuid: str):
+    for todo in Todos:
+        if(todo["uuid"] == uuid):
+            log(("removed ", todo["heading"]), "red")
+            Todos.remove(todo)
+    save(Todos, "todos.txt")
+    return {"response": "Successful"}
+
+@app.delete("/del-project")
+async def del_project(title: str):
+    title = title.title()
+    for project in Projects:
+        if(project["title"] == title):
+            log(("removed ", title), "red")
+            Projects.remove(project)
+    save(Projects, "projects.txt")
+    return {"response": "Successful"}
+
+Todos, Projects = Startup()
