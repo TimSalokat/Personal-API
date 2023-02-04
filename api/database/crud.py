c99c8037f2b7cb05802e1e27c16fd4d198f927bd
Tim <tim.salokat@outlook.com>
2023-02-04 22:27:27 +0100

    Adding and finishing of projects now adds to the projects numbers

--- api/database/crud.py
+++ api/database/crud.py
@@ -26,6 +26,9 @@ def create_project(db: Session, project: schemas.ProjectCreate):
     db.refresh(db_project)
     return db_project
 
+def get_task(db: Session, task_id: str):
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
+
 def get_tasks(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Task).offset(skip).limit(limit).all()
 
--- api/database/schemas.py
+++ api/database/schemas.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 
 class TaskBase(BaseModel):
-
     title: str
     description: str
     priority: int
